00f57aef7890334fd2eecfeca14bb370cdc7b0c5
Nicolo De Rinaldis <nicoderin98@gmail.com>
2022-07-20 08:34:53 +0200

    update form Mansione

--- src/components/landing.js
+++ src/components/landing.js
@@ -33,15 +33,24 @@ const myForm={
           <hr />
 
             <div className="mb-3">
-              <label example="exampleInputEmail1" className="form-label">Email address</label>
+              <label example="exampleInputEmail1" className="form-label"><strong>Email address</strong></label>
               <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
               <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
-              <label example="exampleInputPassword1" className="form-label">Password</label>
+              <label example="exampleInputPassword1" className="form-label"><strong>Password</strong></label>
               <input type="password" className="form-control" id="exampleInputPassword1"/>
             </div>
-            <button type="submit" className="btn btn-primary flex">Submit</button>
+            <div className="mb-3">
+            <label className="form-label"><strong>Mansione:</strong></label>
+            <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+              <option selected>Choose...</option>
+              <option value="1">Front End</option>
+              <option value="2">Back End</option>
+              <option value="3">Full Stack</option>
+            </select>
+            </div>
+            <button type="submit" className="btn btn-primary flex"><strong>Submit</strong></button>
           </form>
             </div>
           </div>
